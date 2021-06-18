f7c46d069ccf8d16c7b46422dce3061d9e482f1f
sridhant <58651073+sridhant@users.noreply.github.com>
2021-06-18 17:24:53 +0530

    added responsive about section

--- js/app.js
+++ js/app.js
@@ -2,3 +2,37 @@
 
   console.log( window.innerHeight  + 'height' )
   console.log( window.innerWidth  + 'width')
+
+  // const tl = new TimelineLite()
+
+  // tl.fromTo('#homeSvg' , 5 , {
+  //   opacity : 0,
+  //   ease : Power3.easeOut,
+  // })
+
+  gsap.registerPlugin(ScrollTrigger);
+
+  gsap.to("#aboutSvg" , {
+    ScrollTrigger : {
+      trigger : "#aboutSvg",
+      start : "top center",
+      markers : true,
+      toggleActions : "restart pause reverse pause",
+    }
+  },{
+    x : 500,
+    duration : 3
+  })
+
+//   tl.to('#aboutSvg' , {
+//     ScrollTrigger : {
+//       trigger : '#aboutSvg',
+//     }
+// } , {
+//  x : 400,
+//  duration : 3,
+// })
+
+
+
+
